b46f52cc53b44819655836f523ad55ae01afde7a
uke <eugenio63@gmail.com>
2014-04-01 00:55:21 -0300

    Documentation

--- UglifierCssJsGrailsPlugin.groovy
+++ UglifierCssJsGrailsPlugin.groovy
@@ -7,4 +7,12 @@ class UglifierCssJsGrailsPlugin {
     def title = "Css/Js Minifier Resource plugin"
     def description = 'Minify Css/Js assets using yui uglify implementation'
     def documentation = "http://grails.org/plugin/uglifier-css-js"
+    def license = "APACHE"
+    def developers = [
+            [name: "Eugenio Lattanzio", email: "eugenio63@gmail.com"]
+
+    ]
+    def issueManagement = [system: 'github', url: 'https://github.com/eugeniol/uglifier-css-js/issues']
+    def scm = [url: "https://github.com/eugeniol/uglifier-css-js"]
+
 }
